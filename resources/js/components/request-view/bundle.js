71d2e00e1e5ac213a0cccc66aafc037f75e1224e
Andres Borda <ajborda4@misena.edu.co>
2023-02-20 07:57:58 -0500

    Modificacion de los componentes de alpine en solicitudes

--- resources/js/components/request-view/bundle.js
+++ resources/js/components/request-view/bundle.js
@@ -4,8 +4,9 @@ import edit from "./edit.js";
 import item from "./item.js";
 import obs from "./obs.js";
 import create from "./create.js";
+import buttons from "./buttons.js";
 import loadMoreRequests from "./load-more-requests.js";
 import searchBox from "./search-box.js";
 import showProjectInfo from "./show-project-info.js";
 
-export { list, deleteRequest, edit, item, obs, create, loadMoreRequests, searchBox, showProjectInfo };
+export { list, deleteRequest, edit, item, obs, create, loadMoreRequests, searchBox, showProjectInfo, buttons };
