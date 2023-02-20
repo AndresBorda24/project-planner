168c01f5d56b79126cc48afd6cf0a7a11a7e9c21
Andres Borda <ajborda4@misena.edu.co>
2023-02-20 07:57:24 -0500

    Modificacion de las vistas en Solicitudes

--- resources/views/partials/priority/search-box.php
+++ resources/views/partials/priority/search-box.php
@@ -1,17 +1,19 @@
 <div 
-class="w-100 mt-1 mb-2 d-flex justify-content-end align-items-center gap-2 flex-wrap position-relative" 
+class="flex-fill position-relative"
 x-data="requestSearchBox" x-modelable="search" x-model.debounce.500ms="$store.searchBox">
-    <label for="search-request" class="form-label a-little-small m-0">Buscar: </label>
-    <div>
-        <input 
-        type="text" 
-        id="search-request" x-model.debounce.500ms="search"
-        class="form-control form-control-sm d-inline-block" style="width: 250px;">
-        <button type="button" class="btn btn-sm btn-light d-inline-block" @click="search = ''">
+    <div class="d-flex gap-1 align-items-center">
+        <div class="flex-fill">
+            <input
+            placeholder="Buscar"
+            type="text"
+            id="search-request" x-model.debounce.500ms="search"
+            class="form-control form-control-sm">
+        </div>
+        <button type="button" class="btn btn-sm btn-outline-dark" @click="search = ''">
             <i class="bi bi-x a-little-small"></i>
         </button>
     </div>
-    <ul class="list-group a-little-small position-absolute shadow-sm top-100 end-0 me-3 mt-1 overflow-auto" style="width: 280px; z-index: 1; max-height: 300px;">
+    <ul class="list-group a-little-small position-absolute shadow-sm top-100 end-0 me-3 mt-1 overflow-auto" style="z-index: 1; max-height: 300px;">
         <template x-for="request in results" :key="request.id">
             <li 
             type="button" x-text="request.subject" @click="fetchOneRequest( request.id )"
