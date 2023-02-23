78578ccb9cfdcf33fc4977fd1637bd1e083cb230
Andres Borda <ajborda4@misena.edu.co>
2023-02-23 09:11:16 -0500

    Estructura base

---
+++ resources/js/components/request-view/bind-request.js
@@ -0,0 +1,4 @@
+export default () => ({
+    show: false,
+    step: 1,
+});
---
+++ resources/views/partials/priority/bind/step2.php
@@ -0,0 +1,5 @@
+<div class="p-2" x-show="step === 2">
+  <button class="btn btn-warning" @click="step = 1">
+    Volver
+  </button>
+</div>
